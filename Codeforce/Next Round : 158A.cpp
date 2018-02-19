b2a7999b852b9e67d1acf2cb5b3659da716b162d
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-19 16:02:33 +0600

    Create Next Round : 158A.cpp

---
+++ Codeforce/Next Round : 158A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,k,a[100],c=0;
+    cin>>n>>k;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=0)
+        {
+        if(a[i] >= a[k-1])
+        {
+            c++;
+        }
+        }
+    }
+    cout<<c<<endl;
+    return 0;
+}
