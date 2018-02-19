634ad383da9fdf063bb906a6669e58741829064a
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-19 09:55:02 +0600

    Create 59A.c

---
+++ Codeforce/59A.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+ int i;
+ int count_lchar,count_uchar,mid_length;
+ char str[100],first,last[99]={0};
+ count_lchar = 0;
+ count_uchar = 0;
+ scanf("%s",&str);
+ for(i=0;str[i]!='\0';i++)
+  {
+  if (str[i] >= 'a' && str[i] <= 'z')
+  {
+      count_lchar++;
+  }
+  if (str[i] >= 'A' && str[i] <= 'Z')
+  {
+      count_uchar++;
+  }
+  }
+  if(count_uchar>count_lchar)
+  {
+      for(i=0;str[i]!='\0';i++){
+      str[i]&='_';
+      }
+  }
+  if(count_lchar>count_uchar)
+  {
+      for(i=0;str[i]!='\0';i++){
+            str[i]|=' ';
+      }
+  }
+  if(count_lchar==count_uchar)
+  {
+      for(i=0;str[i]!='\0';i++){
+            str[i]|=' ';
+      }
+  }
+  printf("%s\n",str);
+ return 0;
+}
+
