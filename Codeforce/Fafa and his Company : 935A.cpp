90810fbeecac894fc577c943e3d4ae6c7a0bdcc2
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-20 09:50:23 +0600

    Create Fafa and his Company : 935A.cpp

---
+++ Codeforce/Fafa and his Company : 935A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,count=0;
+    cin>>n;
+    if(n>=2)
+    {
+       for(i=2;i<=n;i++)
+       {
+           if(n%i==0)
+           {
+               count++;
+           }
+       }
+       cout<<count<<endl;
+    }
+    else
+    {
+        cout<<"0"<<endl;
+    }
+    return 0;
+}
