1f4f7103343340932c84d9b821ae51a4edb6b342
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-20 13:20:17 +0600

    Create Football : 96A.cpp

---
+++ Codeforce/Football : 96A.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str;
+    int n,i,counter=0,cnt=0;
+    cin>>str;
+    n=str.length();
+    for(i=0;i<n;i++)
+    {
+        if(str[i]=='0')
+        {
+            cnt=0;
+            counter++;
+        }
+        else
+        {
+            counter = 0;
+            cnt++;
+        }
+        if(counter>=7)
+        {
+           cout<<"YES"<<endl;
+           return 0;
+        }
+        else if(cnt>=7)
+        {
+           cout<<"YES"<<endl;
+           return 0;
+        }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}
