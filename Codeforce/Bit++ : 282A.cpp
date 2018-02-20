4a2ff2d16160e9f6d612c5de2784574914032d56
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-20 12:07:14 +0600

    Create Bit++ : 282A.cpp

---
+++ Codeforce/Bit++ : 282A.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str;
+    int n,i,x=0,counterplus,counterminus;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>str;
+        counterplus = 0;
+        counterminus = 0;
+        for(int j=0;j<=2;j++)
+        {
+            if(str[j]!='X')
+            {
+            if(str[j]=='+')
+            {
+                counterplus++;
+            }
+            if(str[j]=='-')
+            {
+                counterminus++;
+            }
+            }
+        }
+        if(counterplus == 2)
+        {
+            x++;
+        }
+        if(counterminus == 2)
+        {
+            x--;
+        }
+    }
+    cout<<x<<endl;
+    return 0;
+}
