dd3ee048dd0a11bb358d9d9d10759bf4198204cb
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-25 13:44:58 +0600

    Create Love Triangle : 939A.cpp

---
+++ Codeforce/Love Triangle : 939A.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, s[5000],counter=0,i,j,k;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cin>>s[i];
+    }
+    for(i=1;i<=n;i++)
+    {
+        j = s[i];
+        if(j==i)
+            continue;
+        k = s[j];
+        if(k==i)
+            continue;
+        if(i==s[k])
+        {
+            cout<<"YES"<<endl;
+            return 0;
+        }
+    }
+   cout<<"NO"<<endl;
+   return 0;
+}
