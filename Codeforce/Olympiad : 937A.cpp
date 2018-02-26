8525be9969e8193679bb5f99ddc879aafc335508
Abdul Alim Shakir <shakir1443@gmail.com>
2018-02-26 14:56:31 +0600

    Create Olympiad : 937A.cpp

---
+++ Codeforce/Olympiad : 937A.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+void removeNumber(int numbers[], int idx, int &size)
+{
+	int i;
+	for(i = idx; i < size - 1; i++)
+		numbers[i] = numbers[i + 1];
+	size--;
+}
+
+void removeDuplicate(int numbers[], int &size)
+{
+	int i, j;
+	int number;
+	for(i = 0; i < size; i++)
+	{
+		number = numbers[i];
+		for(j = i + 1; j < size; j++)
+		{
+			if(number == numbers[j])
+			{
+				removeNumber(numbers, j, size); j--;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+    int i,n,s[601],counter=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }
+    removeDuplicate(s,n);
+    for(i = 0; i <n; i++)
+	  {
+	      if(s[i]!=0)
+          {
+              counter++;
+          }
+	  }
+	  cout<<counter<<endl;
+    return 0;
+}
